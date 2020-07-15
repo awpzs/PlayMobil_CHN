036332f92d18e6998df37fc043916dc37afcb6e5
Shi <awp.eran@gmail.com>
2020-07-15 23:34:17 +0100

    init exp files

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,75 +1,106 @@
-// This is a simple demo script, feel free to edit or delete it
-// Find a tutorial and the list of availalbe elements at:
-// https://www.pcibex.net/documentation/
+PennController.ResetPrefix(null); //Initiates PennController
+//PennController.DebugOff()
+PennController.AddHost("https://raw.githubusercontent.com/awpzs/Playmobil_CHN/master/images/")
 
-PennController.ResetPrefix(null) // Shorten command names (keep this line here)
+Sequence( "information", "initRecorder", "prac", "send", "final" )
 
-// Show the 'intro' trial first, then all the 'experiment' trials in a random order
-// then send the results and finally show the trial labeled 'bye'
-Sequence( "intro", randomize("experiment") , SendResults() , "bye" )
+//PennController.SetCounter( "setcounter" );
 
-
-// What is in Header happens at the beginning of every single trial
-Header(
-    // We will use this global Var element later to store the participant's name
-    newVar("ParticipantName")
-        .global()
+newTrial( "information" ,
+    newHtml("information_CHN", "information_CHN.html")
+        .print()
     ,
-    // Delay of 250ms before every trial
-    newTimer(250)
-        .start()
+    newButton("我同意")
+        .settings.center()
+        .print()
+        .wait()
+
+)
+
+//newTrial("identification" ,
+//    newText("请在下方输入您的Prolific ID（请务必输入此ID，否则无法领取报酬）：")
+//        .settings.center()
+//        .print()
+//    ,
+//    newTextInput("inputID", "请输入您的Prolific ID")
+//        .settings.center()
+//        .log()
+//        .print()
+//    ,
+//    newButton("继续")
+//        .settings.center()
+//        .print()
+//        .wait()    
+//    ,
+//    newVar("ID")
+//        .global()
+//        .set( getTextInput("inputID") )
+//)
+//.log( "ID" , getVar("ID") )
+
+InitiateRecorder("http://myserver/saveVoiceRecordings.php", "Please grant the experiment access to your microphone.").label("initRecorder")
+
+newTrial("instruction",
+        newText("<p>在该实验中，你会先看见一张图片和一个句子，请大声朗读这个句子，然后点击它。</p><p>接下来你会看见另一张图片，请用一个句子描述图片中的变化，并大声朗读出这个句子，然后点击“继续”按钮。</p>")
+            .settings.center()
+            .print()
+        newButton("开始实验")
+            .settings.center()
+            .print()
+            .wait()
+)
+
+Template(
+    GetTable("prac.csv")
+        .setGroupColumn("List"), variable =>
+        newTrial( "prac" ,
+            newMediaRecorder("recorder", "audio")
+                .record()
+            ,
+            newImage("one", variable.FirstDisplay)
+                .print()
+            ,
+            newText("sentence", variable.Context)
+                .settings.center()
+                .bold()
+                .print()
+            ,
+            newSelector()
+                .add( getText("sentence") )
+                .wait()
+            ,
+            clear()
+            ,
+            newImage("two", variable.SecondDisplay)
+                .print()
+            ,
+            newButton("继续")
+                .settings.center()
+                .print()
+                .wait()
+            ,
+            getVoiceRecorder("recorder")
+                .stop()
+            ,
+            getMediaRecorder("recorder").test.recorded()
+  )
+  .log( "ID"     , getVar("ID")    )
+  .log( "List"  , variable.List)
+  .log( "Item"   , variable.Item   )
+  .log( "Box"   , variable.Situation   )
+  .log( "Gender"   , variable.Gender   )
+  .log( "Context"   , variable.Context   )
+  )
+
+SendResults( "send" )
+
+newTrial( "final" ,
+    newText("<p>感谢您的参与！</p>")
+        .print()
+    ,
+    newText("<p><a href='https://www.stir.ac.uk' href='_blank'>点击此处结束实验</a></p>")
+        .print()
+    ,
+    newButton("void")
         .wait()
 )
-.log( "Name" , getVar("ParticipantName") )
-// This log command adds a column reporting the participant's name to every line saved to the results
-
-
-newTrial( "intro" ,
-    newImage("pcibex-logo.png")
-        .size( 150 , 200 )      // Resize the image to 150x250px
-        .print()
-    ,
-    newText("<p>Welcome to the PCIbex demo experiment.</p><p>Please enter your name below and press Enter:</p>")
-        .print()
-    ,
-    newTextInput()
-        .print()
-        .wait()                 // The next command won't be executed until Enter is pressed
-        .setVar( "ParticipantName" )
-        // This setVar command stores the value from the TextInput element into the Var element
-)
-
-
-// This Template command generates as many trials as there are rows in myTable.csv
-Template( "myTable.csv" ,
-    // Row will iteratively point to every row in myTable.csv
-    row => newTrial( "experiment" ,
-        // The trials are minimal: choose a pronoun from a DropDown list
-        newDropDown("pronoun", "...")
-            .before( newText(row.Sentence) )    // Print the sentence to the left of the list
-            .add( row.Pronoun1 )
-            .add( row.Pronoun2 )
-            .shuffle()                          // Randomly order the options in the list (Pronoun1 and Pronoun2)
-            .once()                             // Disable the list after the first selection
-            .print()
-            .wait()
-            .log()                              // Make sure to log the participant's selection
-        ,
-        newButton("Next")
-            .print()
-            .wait()
-    )
-    .log( "Sentence" , row.Sentence )
-    .log( "Pronoun1" , row.Pronoun1 )
-    .log( "Pronoun2" , row.Pronoun2 )
-    // Add these three columns to the results lines of these Template-based trials
-)
-
-
-// Spaces and linebreaks don't matter to the script: we've only been using them for the sake of readability
-newTrial( "bye" ,
-    newText("Thank you for your participation!").print(),
-    newButton().wait()  // Wait for a click on a non-displayed button = wait here forever
-)
-.setOption( "countsForProgressBar" , false )
-// Make sure the progress bar is full upon reaching this last (non-)trial
